1974bcad4c941da46062829217536f58c8686889
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-04-10 17:16:14 +0100

    Testing CD with main

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
-        <p>TEST</p>
+        <p>Added CD with main branch</p>
       </div>
     </div>
   );
