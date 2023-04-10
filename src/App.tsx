8d3be0994fe5562832a7e0ad00d4fd723a0ff342
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-04-10 15:30:02 +0100

    Back on a track

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
-        <p>Some change</p>
       </div>
     </div>
   );
